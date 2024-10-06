fc8573fb8740750c259f2bbb76f2a34d46e5f152
boudhomme <123665820+boudhomme@users.noreply.github.com>
2024-10-06 22:12:20 +0200

    Add files via upload

---
+++ header.php
@@ -0,0 +1,20 @@
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+<a class="navbar-brand"><h2>Ewan POCHOLLE</h2></a>
+    <!-- <a class="navbar-brand" h>Erwan POCHOLLE</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span> -->
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link" href="#experience">Expérience</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#formation">Formation</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#competences">Compétences</a>
+        </li>
+      </ul>
+    </div>
+  </nav>
---
+++ portfolio.js
@@ -0,0 +1,35 @@
+// const experienceItems = document.querySelectorAll('.experience-item');
+
+// experienceItems.forEach(item => {
+//   item.addEventListener('mouseover', () => {
+//     item.querySelector('.recap').style.display = 'block';
+//   });
+
+//   item.addEventListener('mouseout', () => {
+//     item.querySelector('.recap').style.display = 'none';
+//   });
+// });
+
+// const experience_Items = document.querySelectorAll('.experience-item');
+
+// experience_Items.forEach(item => {
+//   let recap = item.querySelector('.recap');
+
+//   item.addEventListener('click', () => {
+//     if (recap.style.display === 'none') {
+//       recap.style.display = 'block';
+//     } else {
+//       recap.style.display = 'none';
+//     }
+//   });
+
+//   item.addEventListener('mouseout', () => {
+//     recap.style.display = 'none';
+//   });
+// });
+
+
+
+
+
+
